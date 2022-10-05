57bb76018be5b100867fbe9a059c4f9d465507be
Allen West <58156836+allenwest24@users.noreply.github.com>
2022-10-05 19:32:45 -0400

    Add files via upload

--- hot_chicken.py
+++ hot_chicken.py
@@ -1,23 +1,48 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import sqlite3 as sql
+import json
 
+DB = "./database.db"
 app = Flask(__name__)
 
+# GET endpoint.
 @app.route("/")
 def welcome_screen():
     return "<p>Hot chicken rankings!</p>"
 
+# GET endpoint.
 @app.route('/leaderboard')
-def projects():
-    return 'The best of the hot chickens'
+def loaderboard():
+    con = sql.connect(DB)
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("create table if not exists Meals (RATING real, DISH text, RESTURAUNT text, LOCATION text)")
+    cur.execute("select * from Meals")
+    rows = cur.fetchall()
+    con.commit()
+    con.close()
+    return json.dumps( [dict(x) for x in rows] )
 
+# POST endpoint.
 @app.route('/bounties')
-def about():
-    return 'My most wanted chickens'
+def bounties():
+    input_json = request.get_json(force = True)
+    dictToReturn = {'text':input_json['text']}
+    return jsonify(dictToReturn)
 
-@app.route('/judge')
-def projects():
-    return 'Time to rank some hot chickens'
+# PUT endpoint.
+@app.route('/judge', methods = ['POST'])
+def judge():
+    input_json = request.get_json(force = True)
+    dictToReturn = {'text':input_json['text']}
+    return jsonify(dictToReturn)
 
+# POST endpoint.
 @app.route('/suggest')
-def about():
-    return 'Leave me a suggestion of who to eat next'
+def suggest():
+    input_json = request.get_json(force = True)
+    dictToReturn = {'text':input_json['test']}
+    return jsonify(dictToReturn)
+    
+if __name__ == '__main__':
+    app.run(debug = True)
