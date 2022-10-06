784560893252159175ba11268e3b433e248850df
Allen West <58156836+allenwest24@users.noreply.github.com>
2022-10-06 19:08:26 -0400

    Add files via upload

--- hot_chicken.py
+++ hot_chicken.py
@@ -5,44 +5,75 @@ import json
 DB = "./database.db"
 app = Flask(__name__)
 
-# GET endpoint.
+# GET endpoint that just lists the purpose of the app and its different endpoints.
 @app.route("/")
 def welcome_screen():
-    return "<p>Hot chicken rankings!</p>"
+    return "<p>Hot chicken ratings!</p>"
 
-# GET endpoint.
-@app.route('/leaderboard')
-def loaderboard():
+# GET endpoint to list all current ratings of chickens.
+@app.route('/hot_chickens')
+def hot_chickens():
     con = sql.connect(DB)
     con.row_factory = sql.Row
     cur = con.cursor()
-    cur.execute("create table if not exists Meals (RATING real, DISH text, RESTURAUNT text, LOCATION text)")
-    cur.execute("select * from Meals")
+    cur.execute("create table if not exists Ratings (RATING real, DISH text, RESTURAUNT text, LOCATION text)")
+    cur.execute("select * from Ratings")
     rows = cur.fetchall()
     con.commit()
     con.close()
     return json.dumps( [dict(x) for x in rows] )
 
-# POST endpoint.
-@app.route('/bounties')
-def bounties():
+# POST endpoint to add a new chicken dish to the list to be rated by everyone.
+# $ curl -X POST -H "Content-type: application/json" -d "{\"mealName\" : \"Nashville's Hottest Chicken\", \"resturaunt\" : \"Mr. Cluckers\", \"location\" : \"Nashville, TN\"}" "localhost:5000/new_chicken_rate"
+@app.route('/new_chicken_rate')
+def new_chicken_rate():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        return json
+    else:
+        return 'Content-Type not supported!'
+
+# PUT endpoint to judge existing ranked chicken meals that have already been rated before.
+@app.route('/rate_exisitng_chicken', methods = ['PUT'])
+def rate_existing_chicken():
     input_json = request.get_json(force = True)
     dictToReturn = {'text':input_json['text']}
     return jsonify(dictToReturn)
 
-# PUT endpoint.
-@app.route('/judge', methods = ['POST'])
-def judge():
-    input_json = request.get_json(force = True)
-    dictToReturn = {'text':input_json['text']}
-    return jsonify(dictToReturn)
+# POST endpoint to alert the community to the presence of a new hot chicken to consume.
+# $ curl -X POST -H "Content-type: application/json" -d "{\"mealName\" : \"Nashville's Hottest Chicken\", \"resturaunt\" : \"Mr. Cluckers\", \"location\" : \"Nashville, TN\"}" "localhost:5000/chicken_alert"
+@app.route('/chicken_alert', methods = ['POST'])
+def chicken_alert():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        return json
+    else:
+        return 'Content-Type not supported!'
 
-# POST endpoint.
-@app.route('/suggest')
-def suggest():
-    input_json = request.get_json(force = True)
-    dictToReturn = {'text':input_json['test']}
-    return jsonify(dictToReturn)
+# GET endpoint to list all alerts of new chicken dishes to eat and rate.
+@app.route('/list_chicken_alerts')
+def list_chicken_alerts():
+    con = sql.connect(DB)
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("create table if not exists Alerts (RATING real, DISH text, RESTURAUNT text, LOCATION text)")
+    cur.execute("select * from Alerts")
+    rows = cur.fetchall()
+    con.commit()
+    con.close()
+    return json.dumps( [dict(x) for x in rows] )
+
+# GET Endpoint that returns the rating for a specific chicken dish from a specific resturaunt..
+@app.route('/get_specific_chicken', methods = ['GET'])
+def get_specific_chicken():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        return json
+    else:
+        return 'Content-Type not supported!'
     
 if __name__ == '__main__':
     app.run(debug = True)
